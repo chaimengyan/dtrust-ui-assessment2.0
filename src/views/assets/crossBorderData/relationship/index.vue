12add8bd728fc813f652212d5b8636b10107f814
chaimengyan <1773598170@qq.com>
2024-11-10 20:59:18 +0800

    feat: i

--- src/views/assets/crossBorderData/relationship/index.vue
+++ src/views/assets/crossBorderData/relationship/index.vue
@@ -8,62 +8,8 @@
                 <el-input v-model="relationshipForm.description" type="textarea" :rows="2" placeholder="请输入业务活动描述" />
             </el-form-item>
         <!-- <el-button  @click="addEvaluation">添加资产关联组</el-button> -->
-
-        <div class="evaluation-content">
-            <div class="evaluation-item" >
-                <div class="evaluation-item-content">
-                    <span class="mr-12">{{ 1 }}. </span>
-                    涉及的资产：
-                    <el-select v-model="relationshipForm.assets" :disabled="false" placeholder="请选择涉及的资产" clearable multiple filterable class="mr-12 flex">
-                        <el-option
-                            v-for="item in assetsList"
-                            :key="item.projectId"
-                            :label="item.projectName"
-                            :value="item.projectId"
-                        />
-                    </el-select>
-                    <el-button type="primary" icon="el-icon-plus" circle></el-button>
-                    <el-button type="danger" icon="el-icon-delete" circle></el-button>
-                </div>
-
-                <div class="assets-card" v-if="relationshipForm.assets.length !==0">
-                    <div class="assets-card-header">
-                        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-                        <el-button v-if="checkedAssets.length !==0" icon="el-icon-edit" @click="editAssets" circle></el-button>
-                    </div>
-                    <div style="margin: 15px 0;"></div>
-                    <el-checkbox-group v-model="checkedAssets" @change="handleCheckedAssetsChange">
-                        <el-checkbox 
-                            v-for="assets in assetsList.filter(a=>relationshipForm.assets.includes(a.projectId))" 
-                            :label="assets.projectName" 
-                            :key="assets.projectId">{{assets.projectName}}</el-checkbox>
-                    </el-checkbox-group>
-                </div>
-
-                <div class="assets-card" v-if="relationshipForm.assets.length !==0">
-                    <el-form-item :label="$t('businessScenarioManagement.数据处理活动')" >
-                        <el-cascader
-                            v-model="relationshipForm.activitiesIdList"
-                            :placeholder="`${$t('crudCommon.请选择')}${$t('businessScenarioManagement.数据处理活动')}`"
-                            :options="activitiesOptions"
-                            :props="{ multiple: true, emitPath: false}"
-                            :show-all-levels="false"
-                            @change="changeEvent"
-                            filterable></el-cascader>
-                    </el-form-item>
-                    <el-form-item label="">
-                        <el-divider></el-divider>
-                        <ActAnswers
-                            ref="actAnswers"
-                            :activitiesList="relationshipForm.assetsSceneProjectAttributesActivitiesList"
-                            />
-                    </el-form-item>
-                </div>
-                
-                <RelationshipComponent 
-                     />
-            </div>
-        </div>
+            <el-button type="primary" icon="el-icon-plus" circle @click="handleAddQuestion"></el-button>
+            <relation-question v-model="relationshipForm.questionList" />
     </el-form>
     <el-dialog
         :title="$t('fieldManagement.资产信息')" 
@@ -98,17 +44,16 @@
 <script>
 import { getAllAssetsProject } from "@/api/assets/assetsManagement";
 import AssetsInfo from '@/views/assets/crossBorderData/relationship/assetsInfo'
-import QuestionComponent from '@/views/assets/crossBorderData/relationship/question.vue'
+import RelationQuestion from '@/views/assets/crossBorderData/relationship/question.vue'
 import ActAnswers from "@/views/assets/businessScenarioManagement/actAnswers";
 import { activitiesCategoryOptions } from "@/util/enum"
 import { getAllAssetsActivities } from "@/api/assets/dataProcessingActivities";
 import { isArray } from 'lodash'
 import {deepClone} from '@/util/util'
-import RelationshipComponent from '@/views/assets/crossBorderData/relationship/index'
 
 export default {
     name: "relationship",
-    components: {AssetsInfo, RelationshipComponent, QuestionComponent, ActAnswers },
+    components: {AssetsInfo, RelationQuestion, ActAnswers },
     props: {
         qnId: {
             type: String,
@@ -123,7 +68,8 @@ export default {
             description:'',
             assets: '',
             activitiesIdList: [],
-            assetsSceneProjectAttributesActivitiesList: []
+            assetsSceneProjectAttributesActivitiesList: [],
+            questionList: [{id: 1}]
         },
         checkAll: false,
         checkedAssets: [],
@@ -170,6 +116,12 @@ export default {
         getAllAssetsProject() {
             getAllAssetsProject('').then(res => {
                 this.assetsList = res.data.data
+            })
+        },
+
+        handleAddQuestion() {
+            this.relationshipForm.questionList.push({
+                id: Math.random()
             })
         },
 
@@ -299,48 +251,6 @@ export default {
     display: flex;
     flex-direction: column;
     // justify-content: flex-end;
-    .evaluation-content {
-        margin-top: 40px;
-        border: 1px solid #c7c7c7;
-        border-radius: 4px;
-        box-shadow: 2px 2px 4px #c7c7c7;
-        .evaluation-item {
-            padding: 12px;
-            white-space: nowrap;
-            color: #000;
-
-            border-bottom: 1px solid #eee;
-
-            
-            .evaluation-item-content {
-                display: flex;
-                align-items: center;
-                margin-bottom: 12px;
-
-                .icon {
-                    font-size: 20px;
-                    cursor: pointer;
-
-                    &.primary {
-                        color: #409eff;
-                    }
-
-                    &.error {
-                        color: red;
-                    }
-                }
-            }
-
-            .flex {
-                width: 0;
-                flex: 1;
-            }
-
-            .mr-12 {
-                margin-right: 12px;
-            }
-        }
-    }
 
 }
 </style>
--- src/views/assets/crossBorderData/relationship/question.vue
+++ src/views/assets/crossBorderData/relationship/question.vue
@@ -1,308 +1,169 @@
 <template>
     <div>
-        <div v-for="(b, index) in blockList" :key="b.id">
-            <div class="evaluation-item-block">
-                <div class="evaluation-item-block-title">
-                    <template v-if="!isView" >
-                        <el-button size="small" class="ml-2" @click="addChild(b)">添加逻辑组</el-button>
-                        <el-button size="small" class="mr-2" @click="addConditions(b.id)">添加条件</el-button>
-                    </template>
-                    <el-button type="danger" icon="el-icon-delete" @click="removeLogicGroup(b.id)" circle></el-button>
+        <div v-for="(item, i) in value" :key="item.id" class="evaluation-content">
+            <div class="evaluation-item" >
+                <div class="evaluation-item-content">
+                    <span class="mr-12">{{ newIndex(i) }}. </span>
+                    涉及的资产：
+                    <!-- <el-select v-model="relationshipForm.assets" :disabled="false" placeholder="请选择涉及的资产" clearable multiple filterable class="mr-12 flex">
+                        <el-option
+                            v-for="item in assetsList"
+                            :key="item.projectId"
+                            :label="item.projectName"
+                            :value="item.projectId"
+                        />
+                    </el-select> -->
+                    <el-button type="primary" icon="el-icon-plus" circle @click="handleAdd(item)"></el-button>
+                    <el-button type="danger" icon="el-icon-delete" circle @click="handleDel(item)"></el-button>
                 </div>
-                <div class="evaluation-item-content" >
-                    <el-row :gutter="20"  class="mr-12 flex" >
-                        <el-col :span="4">
-                            <el-select 
-                                :disabled="isView"
-                                v-model="qt.triggerType" 
-                                placeholder="请选择触发条件类型" 
-                                size="small">
-                                <el-option
-                                    v-for="item in triggerTypeOptions"
-                                    :key="item.value"
-                                    :label="item.label"
-                                    :value="item.value"
-                                />
-                            </el-select>
-                        </el-col>
-                        <el-col v-if="qt.triggerType !== 1" :span="6">
-                            <el-select 
-                                :disabled="isView"
-                                :placeholder="`请选择问题`" 
-                                v-model="qt.questionId" 
-                                size="small">
-                                <el-option
-                                    v-for="item in questionList"
-                                    :key="item.id"
-                                    :label="item.title"
-                                    :value="item.id"
-                                />
-                            </el-select>
-                        </el-col>
-                        <el-col :span="4">
-                            <el-select :disabled="isView" v-model="qt.expression" size="small">
-                                <el-option
-                                    v-for="item in Expression[qt.triggerType]"
-                                    :key="item.value"
-                                    :label="item.label"
-                                    :value="item.value"
-                                />
-                            </el-select>
-                        </el-col>
-                        <el-col :span="4">
-                            <template v-if="qt.triggerType === 2">
-                                <el-select :disabled="isView" size="small" v-model="qt.value">
-                                    <el-option
-                                        v-for="item in qtOption(qt.questionId)"
-                                        :key="item.value"
-                                        :label="item.label"
-                                        :value="item.value"
-                                    />
-                                </el-select>
-                            </template>
-                        </el-col>
-                    </el-row>
-                    <template v-if="!isView" >
-                        <el-button type="warning" icon="el-icon-plus" @click="addConditions(b.id)" circle></el-button>
-                        <el-button type="danger" icon="el-icon-delete" @click="removeConditions(b.id, qt)" circle></el-button>
-                    </template>
-                </div>
-        
-                <Question 
-                    :ref="childRef" 
-                    :isView="isView"
-                    :level="level + 1" 
-                    :questionList="questionList" 
-                    :logicGroupId="b.id" 
-                    :evaluation="evaluation" 
-                    :bindBlockList="b.groups" />
+
+                <relation-question v-model="item.children" :index="newIndex(i)" @input="onFlush" />
+<!-- 
+                <div class="assets-card" v-if="relationshipForm.assets.length !==0">
+                    <div class="assets-card-header">
+                        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+                        <el-button v-if="checkedAssets.length !==0" icon="el-icon-edit" @click="editAssets" circle></el-button>
+                    </div>
+                    <div style="margin: 15px 0;"></div>
+                    <el-checkbox-group v-model="checkedAssets" @change="handleCheckedAssetsChange">
+                        <el-checkbox 
+                            v-for="assets in assetsList.filter(a=>relationshipForm.assets.includes(a.projectId))" 
+                            :label="assets.projectName" 
+                            :key="assets.projectId">{{assets.projectName}}</el-checkbox>
+                    </el-checkbox-group>
+                </div> -->
+<!-- 
+                <div class="assets-card" v-if="relationshipForm.assets.length !==0">
+                    <el-form-item :label="$t('businessScenarioManagement.数据处理活动')" >
+                        <el-cascader
+                            v-model="relationshipForm.activitiesIdList"
+                            :placeholder="`${$t('crudCommon.请选择')}${$t('businessScenarioManagement.数据处理活动')}`"
+                            :options="activitiesOptions"
+                            :props="{ multiple: true, emitPath: false}"
+                            :show-all-levels="false"
+                            @change="changeEvent"
+                            filterable></el-cascader>
+                    </el-form-item>
+                    <el-form-item label="">
+                        <el-divider></el-divider>
+                        <ActAnswers
+                            ref="actAnswers"
+                            :activitiesList="relationshipForm.assetsSceneProjectAttributesActivitiesList"
+                            />
+                    </el-form-item>
+                </div> -->
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
-import Question from '@/views/assets/crossBorderData/relationship/question.vue'
 
 export default {
-    name: "question",
-    components: { Question },
+    name: "relation-question",
     props: {
-        isView: {
-            type: Boolean,
-            default: false
-        },
-        questionList: {
+        value: {
             type: Array,
             default: () => []
         },
-        bindBlockList: {
-            type: Array,
-            default: () => []
-        },
-        level: {
-            type: Number,
-            default: 1
-        },
-        evaluation: {
-            type: Object,
-            default: () => {}
-        },
-        logicGroupId: {
+        index: {
             type: Number,
             default: 0
         }
-       
     },
     data() {
       return {
-        blockList: [],
-        triggerTypeOptions: [{
-            label: '题目分数',
-            value: 0
-        },
-        {
-            label: '总分',
-            value: 1
-        },
-        {
-            label: '题目选项',
-            value: 2
-        }],
-        logicTypeOptions: [ {
-            label: '全部',
-            value: 'and'
-        },
-        {
-            label: '任意',
-            value: 'or'
-        }],
-        qnList: [],
+      
       }
     },
     mounted() {
     },
     methods: {
-        qtType(id) {
-            const item = this.questionList.find(item => item.id === id);
-            
-            return item?.componentType;
+        newIndex(i) {
+            return this.index ? `${this.index}-${i + 1}` : i + 1
         },
-
-
-        qtOption(id) {
-            const item = this.questionList.find(item => item.id === id);
-            return item?.options || [];
+        change(value) {
+            this.$emit('input', value)
+            console.log(value, 'v')
         },
-
-        findIndex(id) {
-            return this.blockList.findIndex(qt => qt.id === id)
-        },
-
-        addConditions(id) {
-            const index = this.findIndex(id);
-            if (index !== -1) {
-                const group = this.blockList[index];
-                // if (group.groups?.length) {
-                //     return ElMessage.warning('请删除分组，再添加条件')
-                // } 
-                // 调用接口替换
-                const data = createConditions(this.evaluation.id, group.id)
-                data = data.toString();
-                addConditionApi(data).then(res => {
-                    const { id } = res.data || {};
-                    data.id = id;
-                    if (!group.conditions) {
-                        group.conditions = []
-                    }
-                    group.conditions.push(data)
-                })
-            }
-        },
-        removeLogicGroup(id) {
-            removeLogicApi(id).then(() => {
-                const index = this.blockList.findIndex(item => item.id === id);
-                if (index !== -1) {
-                    this.blockList.splice(index, 1);
-                }
-            })
-        },
-
-        updateLogicGroup(group) {
-            // putLogicApi({ id: group.id, logic: group.logic ? LogicType.and : LogicType.or, count: group.count === null ? null : group.count } as Logic)
-        },
-
-
-        removeConditions(id, qt) {
-            const index = findIndex(id);
-            if (index !== -1) {
-                const list = this.blockList[index].conditions
-                const i = list.findIndex(item => item.id === qt.id)
-                if (i !== -1) {
-                    delConditionApi(qt.id).then(() => {
-                        // 调用接口替换
-                        list.splice(i, 1)
-                        
-                        if (!list.length && !this.blockList[index].groups?.length) {
-                            this.blockList.splice(index, 1)
-                            this.removeLogicGroup(id)
-                        }
-                    })
-                }
-            }
-        },
-
-
-        addChild(item) {
-            // if (item.conditions?.length) {
-            //     return ElMessage.warning('请删除条件，再添加逻辑分组')
-            // }
-            this.$refs.childRef[item.id].addBlock();
-        },
-        addBlock() {
-            const data = this.createLogic();
-            addLogicApi(data).then(res => {
-                const { groups, conditions, ...result } = res.data || {}
-                this.blockList.push({  groups: groups || [], conditions: conditions || [], ...result })
-            })
-        },
-
-
-        updateConditions(item) {
-            this.putConditionApi(item);
-        },
-
-        createConditions(prefabricateId, logicGroupId) {
+        createObject() {
             return {
-                prefabricateId, 
-                questionId: '',
-                logicGroupId,
-                expression: '',
-                triggerType: 0,
-                value: '0',
+                id: Math.random()
             }
         },
-
-        createLogic() {
-            return {
-                id: '',
-                prefabricateId: this.evaluation.id,
-                level: this.level,
-                logicGroupId: this.logicGroupId,
-                logic: LogicType.or,
-                count: null
+        handleAdd(record) {
+            const index = this.value.findIndex(item => item.id === record.id)
+            if (index != -1) {
+                const arr = [...this.value]
+                arr[index].children = arr[index].children ? [] : [...(arr[index].children || [])]
+                arr[index].children.push(this.createObject())
+                this.change(arr)
             }
+           
         },
+        handleDel(record) {
+            const index = this.value.findIndex(item => item.id === record.id)
+            if (index != -1) {
+                const arr = [...this.value]
+                arr.splice(index, 1)
+                this.change(arr)
+            } 
+        },
+        onFlush() {
+            this.change([...this.value])
+        }
     },
 }
 
 </script>
 <style lang="scss" scoped>
-.evaluation-item-block {
-    padding: 12px;
-    margin-bottom: 12px;
-    border: 1px solid #c7c7c7;
-    border-radius: 4px;
-    background-color: white;
-    box-shadow: 2px 2px 4px #c7c7c7;
-    .evaluation-item-block-title {
-        display: flex;
-        align-items: center;
-        margin-bottom: 12px;
-        .icon {
-            font-size: 20px;
-            cursor: pointer;
-            &.error {
-                color: red;
-                margin-left: auto;
+
+    .child-question {
+        margin-top: 0 !important;
+    }
+    .evaluation-content, .child-question {
+        margin-top: 40px;
+        border: 1px solid #c7c7c7;
+        border-radius: 4px;
+        box-shadow: 2px 2px 4px #c7c7c7;
+        padding: 10px;
+        .evaluation-item {
+            padding: 12px;
+            white-space: nowrap;
+            color: #000;
+
+            border-bottom: 1px solid #eee;
+
+            
+            .evaluation-item-content {
+                display: flex;
+                align-items: center;
+                margin-bottom: 12px;
+
+                .icon {
+                    font-size: 20px;
+                    cursor: pointer;
+
+                    &.primary {
+                        color: #409eff;
+                    }
+
+                    &.error {
+                        color: red;
+                    }
+                }
+            }
+
+            .flex {
+                width: 0;
+                flex: 1;
+            }
+
+            .mr-12 {
+                margin-right: 12px;
             }
         }
     }
-    
-    .evaluation-item-content {
-        display: flex;
-        align-items: center;
-        margin-bottom: 12px;
 
-        .icon {
-            font-size: 20px;
-            cursor: pointer;
-
-            &.primary {
-                margin-left: auto;
-                color: #409eff;
-            }
-
-            &.error {
-                color: red;
-            }
-        }
-    }
-
-   
-}
-
-.mb-12 {
-    margin-bottom: 12px;
-}
 </style>
